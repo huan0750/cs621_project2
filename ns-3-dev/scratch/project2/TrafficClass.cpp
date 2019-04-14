6bbdf90bee79fd3045d5740c95f7cd6f0fccb2e9
huan0750 <31259665+huan0750@users.noreply.github.com>
2019-04-13 21:50:07 -0700

    traffic class

--- ns-3-dev/scratch/project2/TrafficClass.cpp
+++ ns-3-dev/scratch/project2/TrafficClass.cpp
@@ -3,14 +3,13 @@
 #include "TrafficClass.h"
 
 
+
 //constructor
 
 TrafficClass::TrafficClass(bool isDefault){
      this->isDefault = isDefault;
 
 }
-
-
 
 
 bool TrafficClass::Enqueue(Ptr<Packet> p){
@@ -25,6 +24,54 @@ Ptr<Packet> TrafficClass::Dequeue(){
     }
 
 }
+
+//– bytes - the count of bytes in the queue.
+//– packets - the count of packets in the queue.
+//– maxPackets - the maximum number of packets allowed in the queue.
+//– maxBytes - the maximum number of bytes allowed in the queue.
+
+void TrafficClass::setBytes(uint32_t num){
+    this->bytes = num;
+
+}
+
+void TrafficClass::setPackets(uint32_t num){
+    this->packets = num;
+
+}
+
+void TrafficClass::setMaxPackets(uint32_t num){
+    this->max_packets = num;
+
+}
+
+void TrafficClass::setMaxBytes(uint32_t num){
+    this->max_bytes = num;
+
+}
+
+void TrafficClass::setPriorityLevel(uint32_t num){
+    this->priority_level = num;
+}
+
+void TrafficClass::setWeight(double_t num){
+	this->weight = num;
+}
+
+
 bool TrafficClass::match(Ptr<Packet> p){
+    bool check = true;
+    for(unsigned int i=0; i<filters.size(); i++){
+        for(unsigned int j=0; j<filters[i]->element.size(); j++){
+            if(filters[i]->element[j]->match(p) == false){
+                check = false;
+                break;
+            }
+        }
+        if(check)
+            return true;
+    }
+
+    return false;
 
 }
--- ns-3-dev/scratch/project2/TrafficClass.h
+++ ns-3-dev/scratch/project2/TrafficClass.h
@@ -10,18 +10,35 @@
 #include <stdint.h>
 #include <queue>
 
+#include "Filter.h"
+
 using namespace ns3;
 
 class TrafficClass {
 private:
-    uint32_t bytes;
+    ns3::Filter* fil;
     std::queue<Ptr<Packet>> m_queue;
+    uint32_t bytes;
+    uint32_t packets;
+    uint32_t max_bytes;
+    uint32_t max_packets;
     bool isDefault;
+    double_t weight;
+    uint32_t priority_level;
+    std::queue<Ptr<Packet> > m_queue;
+    std::vector<ns3::Filter*> filters;
 
 
 public:
     TrafficClass(bool isDefault);
     bool Enqueue(Ptr<Packet> p);
     Ptr<Packet> Dequeue();
+    void setBytes(uint32_t num);
+    void setPackets(uint32_t num);
+    void setMaxPackets(uint32_t num);
+    void setMaxBytes(uint32_t num);
+    void setWeight(double_t num);
+    void setPriorityLevel(uint32_t num);
+    void setIsDefault(bool check);
     bool match(Ptr<Packet> p);
 }
