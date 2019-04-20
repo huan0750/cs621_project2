499505acabd2e7a34310d80fb797884d04b9160f
Ni4991 <geraldthepie@yahoo.com>
2019-04-20 01:41:52 -0700

    filterelements change

---
+++ ns-3-dev/scratch/project2/ProtocolNum.cc
@@ -0,0 +1,32 @@
+#include "ProtocolNum.h"
+#include "ns3/ppp-header.h"
+#include "ns3/ipv4-header.h"
+#include "ns3/udp-header.h"
+#include "ns3/tcp-header.h"
+
+
+
+
+namespace ns3 {
+    bool ProtocolNum::match(Ptr<Packet> p){
+		    Ptr<Packet> copy = p->Copy ();
+            PppHeader ppp;
+			copy->RemoveHeader (ppp);
+			Ipv4Header iph;
+			copy->RemoveHeader (iph);
+			
+           //std::cout <<"start print header "<<std::endl;
+		   //iph.Print(std::cout);
+		    //std::cout<< protocol<<"end print header"<<int(iph.GetProtocol())<<std::endl;
+		
+            if (protocol == "TCP" && iph.GetProtocol() == 0x06){
+				return true;	
+			}else if (protocol == "UDP" && iph.GetProtocol() == 0x11){
+				return true;		
+			}else{
+				return false;
+			}
+        return false;
+    }
+}
+
