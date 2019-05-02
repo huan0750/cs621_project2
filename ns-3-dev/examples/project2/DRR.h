768362de6134dade14fb20a23667a066db3f9c4b
muranxiao <31270586+muranxiao@users.noreply.github.com>
2019-05-02 11:37:45 -0700

    DRR content

---
+++ ns-3-dev/examples/project2/DRR.h
@@ -0,0 +1,41 @@
+#ifndef CS621_PROJECT2_SPQ_H
+#define CS621_PROJECT2_SPQ_H
+
+#include "diffserv.h"
+
+namespace ns3 {
+	
+template <typename Item>
+	class DRR : public  DiffServ<Item> {
+		private:
+        using DiffServ<Item>::q_class;
+		std::vector <double_t> quantums;
+		int turn = 0;
+	  
+	  
+		public:
+        DRR();
+		static TypeId GetTypeId (void);
+		Ptr<Packet> Schedule();
+		void nextTurn();
+	};
+  
+  
+
+template <typename Item>
+TypeId
+DRR<Item>::GetTypeId (void)
+{
+  static TypeId tid = TypeId (("ns3::DRR<" + GetTypeParamName<DRR<Item> > () + ">").c_str ())
+    .SetParent<Queue<Item> > ()
+    .SetGroupName ("Network")
+    .template AddConstructor<DiffServ<Item> > ()
+    .AddAttribute ("ConfigPath",
+                    "The path of config file.",
+                    StringValue ("drr.json"),
+					
+                    MakeStringAccessor(&DiffServ<Item>::LoadConfig),
+                    MakeStringChecker())
+  ;
+  return tid;
+}
